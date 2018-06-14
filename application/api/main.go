7eb102ecc0c4772d015b64f7920afb782ef4f4bf
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-06-14 20:41:11 +0700

    mysql interface

--- application/api/main.go
+++ application/api/main.go
@@ -5,7 +5,19 @@ import (
     "log"
     "os"
     "fmt"
+    "database/sql"
+    "github.com/gorilla/mux"
+    _ "github.com/go-sql-driver/mysql"
 )
+
+type App struct {
+    Router *mux.Router
+    DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) { }
+
+func (a *App) Run(addr string) { }
 
 // main function to boot up everything
 func main() { 
@@ -14,6 +26,14 @@ func main() {
         log.Fatal("Error loading .env file")
     }
 
-    name := os.Getenv("NAME")
-    fmt.Printf("%s %s", "Owl Notification", name)
+    mysqlUsername := os.Getenv("MYSQL_USERNAME")
+    mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+    mysqlDefaultDb := os.Getenv("MYSQL_DEFAULT_DB")
+
+    fmt.Printf("%s", mysqlUsername)
+
+    a := App{} 
+    a.Initialize(mysqlUsername, mysqlPassword, mysqlDefaultDb)
+
+    a.Run(":8080")
 }

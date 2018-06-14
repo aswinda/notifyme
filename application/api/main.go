580aa743702b1e57413bae37e3554476549a0ab3
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-06-14 22:25:57 +0700

    user api

--- application/api/main.go
+++ application/api/main.go
@@ -5,19 +5,7 @@ import (
     "log"
     "os"
     "fmt"
-    "database/sql"
-    "github.com/gorilla/mux"
-    _ "github.com/go-sql-driver/mysql"
 )
-
-type App struct {
-    Router *mux.Router
-    DB     *sql.DB
-}
-
-func (a *App) Initialize(user, password, dbname string) { }
-
-func (a *App) Run(addr string) { }
 
 // main function to boot up everything
 func main() { 
@@ -29,11 +17,13 @@ func main() {
     mysqlUsername := os.Getenv("MYSQL_USERNAME")
     mysqlPassword := os.Getenv("MYSQL_PASSWORD")
     mysqlDefaultDb := os.Getenv("MYSQL_DEFAULT_DB")
+    mysqlHost := os.Getenv("MYSQL_HOST")
+    mysqlPort := os.Getenv("MYSQL_PORT")
 
     fmt.Printf("%s", mysqlUsername)
 
     a := App{} 
-    a.Initialize(mysqlUsername, mysqlPassword, mysqlDefaultDb)
+    a.Initialize(mysqlUsername, mysqlPassword, mysqlDefaultDb, mysqlHost, mysqlPort)
 
     a.Run(":8080")
 }

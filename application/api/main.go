081394a9a5ae41eac8f3428b697c354207e13591
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-04 18:21:16 +0700

    update main go

--- application/api/main.go
+++ application/api/main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-    "github.com/joho/godotenv"
-    "log"
-    "os"
-    "fmt"
+	"net/http"
 )
 
-// main function to boot up everything
-func main() { 
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-
-    mysqlUsername := os.Getenv("MYSQL_USERNAME")
-    mysqlPassword := os.Getenv("MYSQL_PASSWORD")
-    mysqlDefaultDb := os.Getenv("MYSQL_DEFAULT_DB")
-    mysqlHost := os.Getenv("MYSQL_HOST")
-    mysqlPort := os.Getenv("MYSQL_PORT")
-
-    fmt.Printf("%s", mysqlUsername)
-
-    a := App{} 
-    a.Initialize(mysqlUsername, mysqlPassword, mysqlDefaultDb, mysqlHost, mysqlPort)
-
-    a.Run(":8080")
+func main() {
+	http.ListenAndServe(":8080", ChiRouter().InitRouter())
 }
+
+

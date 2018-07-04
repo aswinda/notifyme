f202a61d13cccd139925c769b605f8ea59bd534f
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-04 23:05:10 +0700

    circuit breaker

--- application/api/repositories/UserRepository.go
+++ application/api/repositories/UserRepository.go
@@ -6,6 +6,33 @@ import (
 	"github.com/aswinda/notifyme/models"
 )
 
+type UserRepositoryWithCircuitBreaker struct {
+	UserRepository interfaces.IUserRepository
+}
+
+type UserRepository struct {
+	interfaces.IDbHandler
+}
+
+func (repository *UserRepositoryWithCircuitBreaker) GetUserDetail(userId int) (models.UserModel, error) {
+	output := make(chan models.UserModel, 1)
+	hystrix.ConfigureCommand("get_user_detail", hystrix.CommandConfig{Timeout: 1000})
+	errors := hystrix.Go("get_user_detail", func() error {
+		user, _ := repository.UserRepository.GetUserDetail(userId)
+
+		output <- user
+		return nil
+	}, nil)
+
+	select {
+	case out := <-output:
+		return out, nil
+	case err := <-errors:
+		println(err)
+		return models.UserModel{}, err
+	}
+}
+
 func (repository *UserRepository) GetUserDetail(userId int) (models.UserModel, error) {
 	row, err := repository.Query(fmt.Sprintf("
 			SELECT
--- application/api/servicecontainer.go
+++ application/api/servicecontainer.go
@@ -7,6 +7,9 @@ import (
 	"github.com/aswinda/notifyme/repositories"
 	"github.com/aswinda/notifyme/infrastructure"
 	"database/sql"
+	"github.com/joho/godotenv"
+    "log"
+    "os"
 )
 
 type IServiceContainer interface {
@@ -16,5 +19,27 @@ type IServiceContainer interface {
 type kernel struct{}
 
 func (k *kernel) InjectUserController() controller.UserController {
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    mysqlUsername := os.Getenv("MYSQL_USERNAME")
+    mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+    mysqlDefaultDb := os.Getenv("MYSQL_DEFAULT_DB")
+    mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlPort := os.Getenv("MYSQL_PORT")
 	
+	connectionString := fmt.Sprintf("%s:%s@tcp(localhost:8082)/%s", user, password, dbname)
+    var err error
+    mysqlConn , err = sql.Open("mysql", connectionString)
+    if err != nil {
+        log.Fatal(err)
+	}
+	
+	mysqlHandler := &infrastructures.MysqlHandler{}
+	mysqlHandler.Conn = mysqlConn
+
+	userRepository := &repositories.UserRepository{mysqlHandler}
+
 }
